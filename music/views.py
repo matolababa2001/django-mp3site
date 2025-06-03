7b6912c26479e94d89899612014cf94a87a068ef
soloboy <matolababa197@gmail.com>
2025-06-03 09:06:38 -0700

    Initial commit: Django MP3 download site

---
+++ music/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Song
+
+def song_list(request):
+    songs = Song.objects.all()
+    return render(request, 'music/song_list.html', {'songs': songs})
