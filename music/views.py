b3f3571e083bf53f03c8aa565c889b6fa39f5891
soloboy <matolababa197@gmail.com>
2025-06-03 09:32:21 -0700

    Enhanced MP3 site: dark mode, search, footer, Elasticsearch ready

--- music/views.py
+++ music/views.py
@@ -2,5 +2,15 @@ from django.shortcuts import render
 from .models import Song
 
 def song_list(request):
-    songs = Song.objects.all()
-    return render(request, 'music/song_list.html', {'songs': songs})
+    query = request.GET.get('q')
+    if query:
+        songs = Song.objects.filter(title__icontains=query)
+    else:
+        songs = Song.objects.all()
+
+    recent_songs = songs.order_by('-id')[:5]  # last 5 added
+    return render(request, 'music/song_list.html', {
+        'songs': songs,
+        'recent_songs': recent_songs,
+        'query': query or ''
+    })
